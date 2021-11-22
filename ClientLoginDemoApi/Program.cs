b4a97d9f3731e2870b73f0b7c8639bb87af17c50
xandrf <xandrf@protonmail.com>
2021-11-22 20:49:02 -0300

    initial commit

---
+++ ClientLoginDemoApi/Program.cs
@@ -0,0 +1,29 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((ctx, c) => { c.WriteTo.Console();});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
